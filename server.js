e91ae95f4431b1116f72b0adf8486619cdd8592f
Radisol Figueiredo <radisolfigueiredo@gmail.com>
2019-04-18 21:17:22 -0300

    teste finalizado

--- server.js
+++ server.js
@@ -30,40 +30,10 @@ app.use(express.static('public'))
 app.use(cors())
 
 app.get('/', (req, res) => {
-	const help = `
-	<pre>
-	  <h1>Vianuvem Frontend Test</h1>
-	  Bem vindo ao teste de frontend da Vianuvem!
-
-	  Esse é um teste simples, elaborado para conhecermos um pouco mais sobre como você trabalha.
-	  Nessa primeira etapa, você precisa consumir o serviço abaixo e criar uma tela conforme a imagem a seguir:
-  
-	  <h2>Imagem da tela vista em computador</h2>
-	  <img src="img/screen_1.png"/>
-	  <h2>Imagem da tela vista em celular</h2>
-	  <img src="img/screen_2.png"/>
-
-	  GET /circles
-		UTILIZAÇÃO:
-		  Obtém todos os "círculos" disponíveis para a criação da tela
-
-	  <ol>
-	  	<h2>Regras</h2>
-		 <li>O projeto deverá ser desenvolvido com Javascript puro e / ou jQuery</li>
-		 <li>Projetos responsivos tendem a ser mais bem avaliados</li>
-		 <li>A API já está criada, e nenhuma alteração nela é permitida</li>
-		 <li>Ao passar o mouse sobre as imagens, deve ser exibido o texto da propriedade "name", como acontece na primeira imagem, com o título Twitter</li>
-		 <li>Todo o desenvolvimento será avaliado</li>
-		 <li>Após o termino, entregue o projeto via Github ou arquivo .zip</li>
-	  </ol>
-   </pre>
-	`
-  
-	res.send(help)
-  })
+})
 
 app.get('/circles', (req, res) => {
-    res.send(circles)
+	res.send(circles)
 })
 
 app.listen(3001, () => {
